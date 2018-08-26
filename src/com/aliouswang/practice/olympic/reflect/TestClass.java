f1fc79fe3e1d608d62cae2c2d623c8e31f30b99a
aliouswang <aliouswang@aliouswangdeMac-mini.local>
2018-08-26 23:54:37 +0800

    init commit

--- src/com/aliouswang/practice/olympic/reflect/TestClass.java
+++ src/com/aliouswang/practice/olympic/reflect/TestClass.java
@@ -3,14 +3,20 @@ package com.aliouswang.practice.olympic.reflect;
 import com.aliouswang.practice.olympic.bean.TestClassLoader;
 import com.aliouswang.practice.olympic.util.L;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestClass {
 
     public static void main(String[] args) {
 
+        List arrayList = new ArrayList<>();
+
         Class<?> clazz = TestClassLoader.class;
+        L.d("after TestClassLoader.class");
         try {
             clazz = Class.forName("com.aliouswang.practice.olympic.bean.TestClassLoader");
-            L.d("before new instance");
+            L.d("after Class.forName");
             TestClassLoader testClassLoader = (TestClassLoader) clazz.newInstance();
             L.d("after new instance");
         } catch (InstantiationException e) {
